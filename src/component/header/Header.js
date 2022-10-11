453535de9f5e3a7f088203bdeefcab0495c5d18e
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-10-11 17:28:57 +0600

    responsive navbar added

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../mix ideas 3.jpg'
+import { BeakerIcon, Bars3Icon, LightBulbIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false)
     return (
         <div className='flex items-center justify-between'>
             <Link to="/">
                 <div className='flex items-center mt-2 ml-3'>
-                    <img className='h-20 w-24' src={logo} alt="" />
+                    {/* <img className='h-20 w-24' src={logo} alt="" /> */}
+                    <LightBulbIcon className='h-10 w-15' icons={LightBulbIcon}></LightBulbIcon>
                     <h1 className='text-2xl font-bold ml-3'>IQ dot com</h1>
                 </div></Link>
             <div className='mr-3'>
-                <Link className='text-2xl text-blue-900 ml-3' to='/'>Home</Link>
-                <Link className='text-2xl text-blue-900 ml-3' to='/topic'>Topic</Link>
-                <Link className='text-2xl text-blue-900 ml-3' to='/statistics'>Statistics</Link>
-                <Link className='text-2xl text-blue-900 ml-3' to='/blog'>Blog</Link>
+
+
+                <Bars3Icon onClick={() => setOpen(!open)} className='h-7 w-8 md:hidden' icons={Bars3Icon}></Bars3Icon>
+
+
+                <div className={` bg-blue-900 p-2 duration-200 ease-in md:bg-white md:flex md:inline absolute right-0 rounded-md mr-7 md:top-3 ${(open) ? 'top-12' : '-top-96'}`}>
+
+
+                    <p> <Link className=' text-white md:text-2xl md:text-blue-900 ml-3' to='/'>Home</Link></p>
+                    <p> <Link className='text-white md:text-2xl md:text-blue-900 ml-3' to='/topic'>Topic</Link></p>
+                    <p><Link className=' text-white md:text-2xl md:text-blue-900 ml-3' to='/statistics'>Statistics</Link></p>
+                    <p><Link className='text-white md:text-2xl md:text-blue-900 ml-3' to='/blog'>Blog</Link></p>
+                </div>
             </div>
         </div>
     );
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -2,12 +2,12 @@ import React from 'react';
 import img from '../../rm373batch13-089.jpg'
 const Home = () => {
     return (
-        <div className='flex mt-6'>
-            <div className=" w-6/12">
+        <div className='md:flex mt-6'>
+            <div className="w-full md:w-6/12">
                 <img className=' h-74' src={img} alt="" />
             </div>
-            <div className=" bg-black">
-                <h1 className=' mt-24 text-7xl font-bold text-white h-full p-5  bg-gradient-to-l'>Make difference with you</h1>
+            <div className="bg-blue-900 md:bg-black">
+                <h1 className=' md:mt-24 text-7xl font-bold text-white h-full p-5  '>Make difference with you</h1>
             </div>
         </div>
     );
